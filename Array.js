dcee110483e7dd9cb2813be686b45a426662f298
prahlad kumawat <prahlad.kumawat230@gmail.com>
2023-01-06 14:10:41 +0530

    array and its method is updated

---
+++ Array.js
@@ -0,0 +1,75 @@
+// let arr = [11 , 22 , "rahul" , "23"];
+
+// console.log(arr[1]);
+
+// let arr2 = [];
+// console.log(arr2);
+
+// arr2[0] = "rahul";
+// arr2[1] = "ra";
+// arr2[2] = "a";
+
+// let arr3 = new Array(12 , 13 , 14);
+// console.log(arr3);
+
+
+// let fruits = ["banana" , "apple" , "orange"];
+// fruits[1] = "cherry";
+// console.log(fruits.length);
+
+
+
+// const person = {a:"John", b:"Doe"}; 
+// console.log(person["a"]);
+
+
+// let fruits = ["banana" , "apple" , "orange"];
+
+// console.log(typeof fruits);
+ 
+// let x = Array.isArray(fruits);
+// console.log(x);
+
+
+
+// Array Methods ->
+
+// toString
+
+// let fruits = ["banana" , "apple" , "orange" , 25 , 50];
+// console.log(`toString : ${fruits.toString()}`);
+
+// join
+
+// console.log(`join : ${fruits.join("|")}`);
+
+
+// pop and push ->
+// let fruits = ["banana" , "apple" , "orange" , 25 , 50];
+
+// fruits.pop();
+// fruits.push("xxxx");
+
+// console.log(fruits);
+
+// shift and Unshift
+// let fruits = ["banana" , "apple" , "orange" , 25 , 50];
+
+// fruits.shift()
+// fruits.unshift("xxxx")
+
+// console.log(fruits);
+
+
+// concat
+let fruits1 = ["banana" , "apple" , "orange"];
+let fruits2 = [25 , 50];
+let fruits3 = [-1 , -50];
+
+
+// let arr = fruits1.concat(fruits2 , "xxx" , "rrrr" , fruits3);
+console.log(arr);
+
+
+
+
