6773830bf5eb1c852668b3a19a685f4a50bd5b9f
prahlad kumawat <prahlad.kumawat230@gmail.com>
2023-01-17 16:28:53 +0530

    Loop chapter is end

---
+++ Loop.js
@@ -0,0 +1,66 @@
+
+// let n =100;
+
+// for(let x=1 ; x<=n ; x++){
+//     console.log(x);
+// }
+
+
+// printing the even number
+
+// let even = 25;
+
+// for(let i =0 ; i < even ; i++){
+//     i % 2 == 0 ? console.log(i) : '';
+// }
+
+// let i = 0;
+// while (i < 0) {
+//     console.log(i);
+//     i++;
+// }
+
+// let i = 2;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<3)
+
+
+// let arr = [12 , 132, 13,21,3];
+
+// for(let x in arr){
+//     console.log(x);
+// }
+
+// for(let x of arr){
+//     console.log(x);
+// }
+
+
+// let obj = {name:"rahul" , age:32};
+
+// for(let x in obj){
+//     console.log(x);
+// }
+
+// Error
+// for(let x of obj){
+//     console.log(x);
+// }
+
+// console.log(obj.name);
+// console.log(obj["name"]);
+
+// for(let x in obj){
+//     console.log(obj[x]);
+// }
+
+let arr = [12 , 132, 13,21,3 , 45 , 45 , 78 ,78];
+
+let l = arr.length;
+
+for(let i =0 ; i < l ; i++){
+    console.log(arr[i]);
+}
+
