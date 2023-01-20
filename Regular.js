de5628bf944198fa988ee62e166bb4200c51a22f
prahlad kumawat <prahlad.kumawat230@gmail.com>
2023-01-20 16:44:07 +0530

    TypeOf and Regular ex file uploaded

---
+++ Regular.js
@@ -0,0 +1,42 @@
+// Match --> array
+// test  --> true/false
+
+// let intro = "My name Is \nRahul is and is";
+
+// i 
+// let arr = intro.match(/is/i);
+
+// g
+// let arr = intro.match(/is/ig);
+
+// ^
+// console.log(intro);
+// let arr = intro.match(/^Rahul/m);
+// let check = /my/i.test(intro);
+// console.log(check);
+
+// []
+// let intro = "My 8755 name Is 5482113 \nRahul is and is";
+
+// let arr = intro.match(/[0-9]/ig);
+// let check = /[a-zA-Z]/g.test(intro);
+// console.log(check);
+// console.log(arr);
+
+// ()
+// let intro = "My 8755 name Is 5482113 \nRahul is and is";
+
+// let check = /(Si|SI)/g.test(intro);
+// console.log(check);
+
+// \d
+
+// let intro = "My 8755 name Is 5482113 Rahul is and is";
+
+// [0-9] == \d
+// let x= /\d/.test(intro);
+// let x= /\s/.test(intro);
+// let arr = intro.match(/\s/g)
+// console.log(arr);
+// console.log(x);
+
